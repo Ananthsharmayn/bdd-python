f63f81bd5966932380da1964e6fc4d3cd57a515e
Ananthsharmayn <117114117+Ananthsharmayn@users.noreply.github.com>
2024-03-11 22:26:00 +0530

    Update my_steps.py

--- features/steps/my_steps.py
+++ features/steps/my_steps.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import time
-#updated in remote
+
 @given('I am on the Career landing page')
 def step_given_i_am_on_career_page(context):
     service=Service(executable_path='/home/ananth/Downloads/chromedriver-linux64/chromedriver')
